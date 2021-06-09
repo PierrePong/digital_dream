58296ec2d613995510479a2aec9fc3cfcd6b9ec2
Jessye Gabella <jessye.seleucide@gmail.com>
2021-06-09 13:09:38 +0200

    tf preproc v2

--- digital_dream/tf_preprocess.py
+++ digital_dream/tf_preprocess.py
@@ -1,6 +1,7 @@
 """Tensorflow smart preprocessing for all our dataset"""
 import tensorflow as tf
 import os
+import matplotlib.pyplot as plt
 
 """the importation from google drive is manual
 you have to run this part in collab separately, connect to google and then retrieve the code
@@ -20,8 +21,15 @@ for root, subdirectories, files in os.walk(directory):
 """all data are in only one batch with no train/validation split but it can be changed,
 also size is 256x256"""
 tfdf_img= tf.keras.preprocessing.image_dataset_from_directory(
-    img_folders, labels=None, label_mode=None,
+    directory, labels=None, label_mode=None,
     class_names=None, color_mode='rgb', batch_size=32, image_size=(256,
     256), shuffle=True, seed=None, validation_split=None, subset=None,
     interpolation='bilinear', follow_links=True, smart_resize=True
 )
+
+plt.figure(figsize=(10, 10))
+for images in tfdf_img.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.axis("off")
