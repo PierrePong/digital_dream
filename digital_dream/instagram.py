dc35b422a41bbce9d03a5c39d61e831323a195bc
Alexey Novikov <exile_88@mail.ru>
2021-06-09 09:26:51 +0200

    Instagram_loader v2. 10 requests per hour, 50 per day

--- digital_dream/instagram.py
+++ digital_dream/instagram.py
@@ -2,11 +2,15 @@ import requests
 import urllib
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 def instagram_downloader(hashtag='marseille'):
     '''
     This function downloads photos with Marseille hashtag from Instagram.com
     '''
+    # Get today's date
+    today = date.today().isoformat()
+    
     # Get API keys from .env file
     load_dotenv()
     api_key = os.getenv('x-rapidapi-key')
@@ -29,12 +33,10 @@ def instagram_downloader(hashtag='marseille'):
     
     url_list = set() # empty set to keep unique URLs leading to photos
     
-    for i in range(25):
-        print(f"Processing request #{i+1}")
+    for i in range(10): # 10 requests per hour MAX (50 per day)!!!
         try:
             r = requests.get(url, headers=headers, params=params).json()
             cursor = r['body']['edge_hashtag_to_media']['page_info']['end_cursor'] # changing cursor to load next photos
-            print(cursor)
         
             # Loop to append all URLs from the request
             for item in r['body']['edge_hashtag_to_media']['edges']:
@@ -44,7 +46,7 @@ def instagram_downloader(hashtag='marseille'):
 
     # Download photos            
     for i, url in enumerate(url_list):
-            urllib.request.urlretrieve(url, f'raw_data/instagram_{i}.jpg')
+            urllib.request.urlretrieve(url, f'raw_data/instagram_{today}_{i}.jpg')
         
 if __name__ == '__main__':
     instagram_downloader()
